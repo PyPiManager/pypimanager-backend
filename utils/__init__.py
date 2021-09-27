87f0dbaf4c6a8798cffd3fef1c4ed683cd76b3fa
toddlerya <toddlerya@gmail.com>
2021-09-27 12:57:07 +0800

    update base tool code

---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+# @Created Date: 2021/09/27 11:58
+# @Author: toddlerya
+# -----
+# @Last Modified: 2021/09/27 11:58
+# @Modified By: toddlerya
+
--- utils/db.py
+++ utils/db.py
@@ -6,13 +6,22 @@
 # @Last Modified: 2021/09/27 10:10
 # @Modified By: toddlerya
 
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.dialects import mysql
 from sqlalchemy.ext.declarative import declarative_base
 
-from settings import SQLALCHEMY_URL, SQLALCHEMY_ECHO, SQLALCHEMY_AUTO_FLUSH, SQLALCHEMY_AUTO_COMMIT
+from pypimanager_backend.settings import SQLALCHEMY_URL, SQLALCHEMY_ECHO, SQLALCHEMY_AUTO_FLUSH, SQLALCHEMY_AUTO_COMMIT
+from pypimanager_backend.utils.log import logger
 
 
 Base = declarative_base()
 
+
+class DB:
+    @logger.catch(reraise=True)
+    def __init__(self):
+        """
+        初始化数据库对象
+        """
